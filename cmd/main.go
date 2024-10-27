3f6e74faa6df85d17f19ada34d9dafab871d31f1
Exc0mmun1cad0 <fauxmen4@gmail.com>
2024-10-27 16:11:27 +0300

    added psql client connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"db-coursework/internal/config"
+	postgresql "db-coursework/pkg/postgresql_client"
+	"log"
+)
+
 func main() {
-	
+	cfg := config.MustLoad()
+
+	conn, err := postgresql.NewClient(cfg.PostgreSQL)
+	if err != nil {
+		log.Fatalf("failed to connect to db: %s", err)
+	}
+
+	_ = conn
 }

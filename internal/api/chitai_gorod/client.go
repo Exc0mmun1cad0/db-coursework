fa30ab1333528804223e3772a9174c2d2fc9122c
Exc0mmun1cad0 <fauxmen4@gmail.com>
2024-11-01 22:49:13 +0300

    added caching of token in txt file and token reusing

--- internal/api/chitai_gorod/client.go
+++ internal/api/chitai_gorod/client.go
@@ -2,6 +2,8 @@ package chitaigorod
 
 import (
 	"net/http"
+
+	"github.com/pkg/errors"
 )
 
 type Client struct {
@@ -9,9 +11,13 @@ type Client struct {
 	httpClient *http.Client
 }
 
-func NewClient(authToken string) *Client {
+func NewClient() (*Client, error) {
+	authToken, err := newAuthToken()
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot receive auth token for chitai gorod API")
+	}
 	return &Client{
 		token:      authToken,
 		httpClient: &http.Client{},
-	}
+	}, nil
 }
--- internal/api/chitai_gorod/token.go
+++ internal/api/chitai_gorod/token.go
@@ -1,21 +1,85 @@
+// Online store API needs token of Bearer format to have access to store information
 package chitaigorod
 
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
+	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/pkg/errors"
 )
 
 var (
-	chitaiGorodURL   = "https://chitai-gorod.ru"
+	chitaiGorodURL = "https://chitai-gorod.ru"
+	// name for the file where auth token will be cached
+	tokenFile = "token.txt"
 )
 
-// Online store API needs token of Bearer format to have access to store information
-func NewAuthToken() (string, error) {
+// NewAuthToken checks whether jwt auth token lies in token.txt file
+// If it doesn't exist, it receives token by making request to chitaigorod main site
+// This is done in order to avoid frequent visits to the site and as a result not get banned
+func newAuthToken() (string, error) {
 	const op = "chitaigorod.AuthToken"
+
+	oldTokenBytes, err := os.ReadFile(tokenFile)
+	if errors.Is(err, os.ErrNotExist) {
+		newToken, err := getTokenFromRequest()
+		if err != nil {
+			return "", errors.Wrap(err, op)
+		}
+
+		err = os.WriteFile(tokenFile, []byte(newToken), 0755)
+		if err != nil {
+			return "", errors.Wrap(err, op)
+		}
+
+		return formatToken(newToken), nil
+	}
+
+	newToken := string(oldTokenBytes)
+
+	expTime, err := parseAuthToken(newToken)
+	if err != nil {
+		return "", errors.Wrap(err, op)
+	}
+
+	now := time.Now().Unix()
+	if now > expTime+5*60 {
+		newToken, err = getTokenFromRequest()
+		if err != nil {
+			return "", errors.Wrap(err, op)
+		}
+		err = os.WriteFile(tokenFile, []byte(newToken), 0755)
+		if err != nil {
+			return "", errors.Wrap(err, op)
+		}
+	}
+
+	return formatToken(newToken), nil
+}
+
+// parseAuthToken parses jwt token and returns the timestamp after which it will expire
+func parseAuthToken(rawToken string) (int64, error) {
+	const op = "chitaigorod.ParseAuthToken"
+
+	tokenString := strings.Replace(formatToken(rawToken), "Bearer ", "", 1)
+	token, _, err := new(jwt.Parser).ParseUnverified(tokenString, jwt.MapClaims{})
+	if err != nil {
+		return 0, errors.Wrap(err, op)
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		return int64(claims["exp"].(float64)), nil
+	}
+
+	return 0, errors.Wrap(fmt.Errorf("no expiration date info in jwt token"), op)
+}
+
+func getTokenFromRequest() (string, error) {
+	const op = "chitaigorod.getTokenFromRequest"
 
 	resp, err := http.Get(chitaiGorodURL)
 	if err != nil {
@@ -29,34 +93,21 @@ func NewAuthToken() (string, error) {
 		}
 	}
 
-	var rawToken string
+	var token string
 	for _, val := range headersList {
 		if strings.Contains(val, "access-token") {
-			rawToken = val
+			token = val
 		}
 	}
 
-	authToken := strings.Split(rawToken, " ")[0]
-	authToken = authToken[strings.Index(authToken, "Bearer"):]
-
-	authToken = strings.Replace(authToken, "%20", " ", 1)
-	authToken = authToken[:len(authToken)-1]
-
-	return authToken, nil
+	return token, nil
 }
 
-// ParseAuthToken parses jwt token and returns the timestamp after which it will expire
-func ParseAuthToken(tokenString string) (int64, error) {
-	const op = "chitaigorod.ParseAuthToken"
+func formatToken(rawToken string) string {
+	tokenString := strings.Split(rawToken, " ")[0]
+	tokenString = tokenString[strings.Index(tokenString, "Bearer"):]
+	tokenString = strings.Replace(tokenString, "%20", " ", 1)
+	tokenString = tokenString[:len(tokenString)-1]
 
-	token, _, err := new(jwt.Parser).ParseUnverified(tokenString, jwt.MapClaims{})
-	if err != nil {
-		return 0, errors.Wrap(err, op)
-	}
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok {
-		return int64(claims["exp"].(float64)), nil
-	}
-
-	return 0, errors.Wrap(fmt.Errorf("no expiration date info in jwt token"), op)
+	return tokenString
 }
